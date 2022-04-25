bb7e2de0cea16e9e668dc1784b3d28ba085912f2
flortaglia <flortagliaferro@gmail.com>
2022-04-25 13:50:50 -0300

    optimización ItemDetail

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -42,49 +42,35 @@ const ItemDetail = ({id,stock, category, title,description,price,pictureUrl}) =>
   
  
   return (
-    <div>
+    
+    <div className="card my-2" style={{width: "18rem"}}>
       
-      <div className="card my-2" style={{width: "18rem"}}>
-        
-            <img src={pictureUrl} className="card-img-top" alt={description}/>  
-          <div className="card-body">
-            <h4 className="card-title">{title}</h4>
-            <p className="card-text">{description}</p>
-            <h5>Precio: $<span> {price}</span></h5>
-          </div>
-         
-      
+      <img src={pictureUrl} className="card-img-top" alt={description}/>  
+      <div className="card-body">
+        <h4 className="card-title">{title}</h4>
+        <p className="card-text">{description}</p>
+        <h5>Precio: $<span> {price}</span></h5>
+      </div>
+      <ItemCount 
+        stock={stock} 
+        initial={1}
+        onAdd={onAdd}
+        />
+    
       {
-        isInCart(id)?
-        <>
-          <div >
-          <ItemCount 
-            stock={stock} 
-            initial={1}
-            onAdd={onAdd}
-           />
-          
+        isInCart(id)
+        &&
+        <div>        
           <Link className="btn btn-success my-3" to="/">
             Seguir Comprando
           </Link>
           <Link className="btn btn-info my-3 mx-2 " style={{color:"white"}} to="/cart">
             Ver Carrito
           </Link>
-
-          </div>
-        </>
-        :
-        <>
-          <ItemCount 
-            stock={stock} 
-            initial={1}
-            onAdd={onAdd}
-           />  
-        </> 
-       
+        </div>        
       }
-     </div>        
-    </div> 
+    </div>        
+    
   )
 }
 
